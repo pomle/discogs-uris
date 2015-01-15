bb3ba5801cd0bbcce68ab42d64a9b971da902b07
Pontus Persson <pom@spotify.com>
2015-01-15 15:00:03 +0100

    Python Discogs release to Spotify URI resolver.
    Change-Id: I960a5ccdd3ed69b245ab3f8c64fd891453d31065

---
+++ import.py
@@ -0,0 +1,33 @@
+import sys
+import requests
+import json
+
+username = sys.argv[1]
+
+url = 'https://api.discogs.com/users/%s/collection/folders/0/releases' % username
+queue = [url]
+
+
+
+while len(queue):
+	url = queue.pop(0)
+	response = requests.get(url, headers={'user-agent': 'Playlist Import'})
+	releases = json.loads(response.text)
+
+	if 'next' in releases['pagination']['urls']:
+		queue.append(releases['pagination']['urls']['next'])
+
+	for release in releases['releases']:
+		title = release['basic_information']['title']
+		artists = []
+		for artist in release['basic_information']['artists']:
+			 artists.append(artist['name'])
+		lookup = title + " " + " ".join(artists)
+
+		response = requests.get('https://api.spotify.com/v1/search', params={'q': lookup, 'type': 'album'})
+		results = json.loads(response.text)
+
+		if len(results['albums']['items']):
+			print results['albums']['items'][0]['uri'], release['instance_id'], lookup
+		else:
+			print >> sys.stderr, "%s not found :(" % lookup
